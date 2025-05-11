fb1e37f933e00796a71fcd7c0b696f7be4c17c28
madswh <whitmi000@gmail.com>
2025-05-11 11:33:42 -0700

    .

--- dockerstuff.sh
+++ dockerstuff.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 #build docker image
 docker build -t myapp:latest .
 #run docker container
-docker run -rm -d -p 80:5001 --name myapp-container myapp:latest
+docker run -d -p 80:5001 --name myapp-container myapp:latest
 #make sure the app actually starts
 python3 app.py
