60378271f5bd47e679c9805302f45d51cd8e909e
madswh <whitmi000@gmail.com>
2025-05-11 11:59:51 -0700

    .

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('./templates/home.html')
+    return render_template("home.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=False)
--- dockerstuff.sh
+++ dockerstuff.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #build docker image
-docker build -t myapp:latest .
+docker build -t brag:latest .
 #run docker container and remove it when it stops
 #run the container in detached mode
 # -d: run in detached mode
@@ -8,6 +8,6 @@ docker build -t myapp:latest .
 # -rm: automatically remove the container when it exits
 # -p 80:5000: map port 80 on the host to port 5000 on the container
 # --name: name the container
-docker run -d -p -rm 80:5000 --name brag-container myapp:latest
+docker run -d -p -rm 80:5000 --name brag-container brag:latest
 #make sure the app actually starts
 python3 app.py
