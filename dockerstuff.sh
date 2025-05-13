66d62ce7d742b8b34a65440e55b1268ae0b7800d
madswh <whitmi000@gmail.com>
2025-05-13 11:54:06 -0700

    .

--- dockerstuff.sh
+++ dockerstuff.sh
@@ -3,6 +3,6 @@
 docker build -t brag:latest .
 
 #run docker image
-docker run -d --name brag -p 80:5001 brag:latest
+docker run -d --rm --name brag -p 80:5001 brag:latest
 #make sure the app actually starts
 python3 app.py
